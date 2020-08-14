bbc4c49650531cfe9e9bd4c99681d8e0156f3725
RivaD2 <rivkadavidowski@gmail.com>
2020-08-14 16:20:22 -0700

    added comments to server.js assets and images folders

--- server.js
+++ server.js
@@ -8,19 +8,27 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
+// app.get('/hello', (request, response) => {
+//   response.status(200).send('Hello');
+// });
 
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
-});
+// app.get('/data', (request, response) => {
+//   let airplanes = {
+//     departure: Date.now(),
+//     canFly: true,
+//     pilot: 'Well Trained',
+//   };
+//   response.status(200).json(airplanes);
+// });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
+/* THINGS WE DON'T NEED:
+lines 11-22 (These were test paths)
+we need everything else
+- We do not index.html file because we have one in our PORTFOLIO
+- ALL WE NEED TO DO:
+- Find where portfolio lives on machine and drag and drop entire folder and make sure everything ended up at the root of public*/
+
